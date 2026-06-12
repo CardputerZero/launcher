e9ee0530d222cdf3368170f196a38962f0f8adb7
dianjixz <18637716021@163.com>
2026-06-12 19:05:29 +0800

    refactor(APPLaunch): remove Launch impl wrapper

--- projects/APPLaunch/main/ui/Launch.h
+++ projects/APPLaunch/main/ui/Launch.h
@@ -7,10 +7,39 @@
 #pragma once
 
 #include "lvgl/lvgl.h"
-#include <memory>
+#include "cp0_lvgl_app.h"
 
-class LaunchImpl;
+#include <functional>
+#include <list>
+#include <memory>
+#include <string>
+
+class Launch;
 class UILaunchPage;
+
+template <class PageT>
+struct page_t
+{
+    using type = PageT;
+};
+
+template <class PageT>
+inline constexpr page_t<PageT> page_v{};
+
+struct app
+{
+    std::string Name;
+    std::string Icon;
+    std::string Exec;
+    std::function<void(Launch *)> launch;
+
+    app(std::string name, std::string icon, std::string exec, bool terminal);
+    app(std::string name, std::string icon, std::string exec, bool terminal, bool sysplause);
+    app(std::string name, std::string icon, std::string exec, bool terminal, bool sysplause, bool run_as_root);
+
+    template <class PageT>
+    app(std::string name, std::string icon, page_t<PageT> tag);
+};
 
 class Launch
 {
@@ -25,6 +54,25 @@ public:
     void launch_app();
 
 private:
-    std::unique_ptr<LaunchImpl> impl_;
+    friend struct app;
+
+    void go_back_home();
+    void launch_Exec_in_terminal(const std::string &exec, bool sysplause = true);
+    void launch_Exec(const std::string &exec, bool keep_root = false);
+    void applications_load();
+    void inotify_init_watch();
+    void refresh_ui_panels();
+    void applications_reload();
+
+    static void lv_go_back_home(void *arg);
+    static void app_dir_watch_cb(lv_timer_t *timer);
+
     std::shared_ptr<UILaunchPage> launch_page_;
+    int current_app = 2;
+    cp0_watcher_t dir_watcher = NULL;
+    lv_timer_t *watch_timer = nullptr;
+    int fixed_count = 0;
+    std::list<app> app_list;
+    std::shared_ptr<void> app_Page;
+    std::shared_ptr<void> home_Page;
 };
